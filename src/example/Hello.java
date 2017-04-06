0aa2a20fb9ee244fcdfa72ab0d6b6b6dfbe94709
Armellys <adele.journee@hotmail.fr>
2017-04-06 09:26:23 +0200

    test sur machine de l'école, ne marche pas non plus

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,19 +1,27 @@
 
 package example;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+import java.lang.Object;
+import java.io.*;
+import com.csvreader.CsvReader;
 import org.apache.commons.lang.WordUtils;
 
 public class Hello {
     public static void main(String[] args) {
 	int monmax = 0;
-	CSVReader reader = new CSVReader(new FileReader("data.csv"));
+	CsvReader reader = new CsvReader(new FileReader("data.csv"));
 	List<String[]> myEntries = reader.readAll();
 	System.out.println("Nombre dans le list : "+myEntries.size());
 	 for(int i = 0; i < myEntries.size(); i++){
 		System.out.println("Élément à l'index " + i + " = " + myEntries.get(i));
-		if (monmax < myEntries.get(i)){
-			monmax = myEntries.get(i);
-		}
+		/*if (monmax < myEntries.get(i)){
+			String s = Arrays.asList(myEntries.get(i)).toString();
+			Integer m = new Integer(s);
+			monmax = m.intValue();
+		}*/
 	}
 	
 	System.out.println("Max : " + monmax);
